699e3bedff97fb65cf9cec7e771eab064eeeebb5
mortalpuppet <misagh.sj@mgail.com>
2023-01-05 03:40:47 +0330

    Created a Baseline Header Menu, todo: style and implement header functionalities

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,18 @@
+import Image from "next/image";
+import HeaderIcon from './HeaderIcon'
+import {HomeIcon, PhoneIcon, UserIcon, InformationCircleIcon} from '@heroicons/react/24/solid'
+import { userAgent } from "next/server";
 
 export default function Header() {
   return (
-    <h1>Header</h1>
+    <div className="flex items-center p-2">
+        <div className="flex space-x-5">
+            <HeaderIcon Icon={HomeIcon} title="Home" />
+            <HeaderIcon Icon={UserIcon} title="Acount" />
+            <HeaderIcon Icon={PhoneIcon} title="Contact" />
+            <HeaderIcon Icon={InformationCircleIcon} title="About" />
+        </div>
+        <Image src={process.env.LOGO_URL} width={100} height={100} alt="movie db logo" className="ml-auto"></Image>
+    </div>
   )
 }
---
+++ components/HeaderIcon.js
@@ -0,0 +1,9 @@
+
+export default function HeaderIcon({Icon, title}) {
+  return (
+    <div className="">
+        <Icon className='h-8'/>
+        <p>{title}</p>
+    </div>
+  )
+}
